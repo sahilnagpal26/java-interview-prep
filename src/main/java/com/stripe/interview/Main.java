4554a03b7ed161412940cf194912d7abd41f2240
Sahil Nagpal <sahilnagpal@stockx.com>
2023-06-21 13:47:56 +0530

    Test changes

--- src/main/java/com/stripe/interview/Main.java
+++ src/main/java/com/stripe/interview/Main.java
@@ -4,7 +4,7 @@ import com.google.common.html.HtmlEscapers;
 
 public class Main {
 	public static void main(String... args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello world 123!");
 	}
 
 	public static String useGuavaForSomeReason(String input) {
